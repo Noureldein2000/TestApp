116f9117ef5564b12426c99f60107e9e7f3e856a
Nour <noureldin@tafeel.com>
2024-05-16 12:26:53 +0300

    Add project files.

---
+++ TestConsoleApp/MutliTask.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    public static class MutliTask
+    {
+        public static string UseThreads(int count, DTO dto)
+        {
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    AnyMethodLogic(MethodBase.GetCurrentMethod().Name);
+                    lock (dto) count++;
+                });
+
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            threads.ForEach(t => t.Join());
+
+            return "Finish Thread";
+        }
+
+        public static string UseTasks(int count, DTO dto)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var task = new Task(() =>
+                {
+                    AnyMethodLogic(MethodBase.GetCurrentMethod().Name);
+                    lock (dto) count++;
+                });
+
+                task.Start();
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            return "Finish Tasks";
+
+        }
+
+        public static string UseParallelFor(int count, DTO dto)
+        {
+            Parallel.For(count, 10, (i) =>
+            {
+                AnyMethodLogic(MethodBase.GetCurrentMethod().Name);
+                lock (dto) count++;
+            });
+            return "Finish Parallel";
+        }
+
+        private static void AnyMethodLogic(string job)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"after sleeep {job}");
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestConsoleApp;
+
+Console.WriteLine("Hello, World!");
+var count = 0;
+var anyObject = new DTO();
+
+Console.WriteLine(MutliTask.UseThreads(count, anyObject));
+
+Console.WriteLine($"will next call job!{nameof(MutliTask.UseTasks)}");
+
+Console.WriteLine(MutliTask.UseTasks(count, anyObject));
+
+Console.WriteLine($"will next call job!{nameof(MutliTask.UseParallelFor)}");
+
+Console.WriteLine(MutliTask.UseParallelFor(count, anyObject));
